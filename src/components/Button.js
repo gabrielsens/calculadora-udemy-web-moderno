303d8e3dc5cd4109bb67b92733d59760bd655806
Gabriel Sens <gabriel_sens@hotmail.com>
2022-03-01 21:12:00 -0300

    .

---
+++ src/components/Button.js
@@ -0,0 +1,15 @@
+import './Button.css';
+
+export default function Button(props) {
+    return (
+        <button
+            onClick={() => props.click && props.click(props.label)} 
+            className={`
+            button 
+            ${props.operation ? 'operation' : ''} 
+            ${props.double ? 'double' : ''}
+            ${props.triple ? 'triple' : ''}`}>
+            {props.label}
+        </button>
+    )
+} 
